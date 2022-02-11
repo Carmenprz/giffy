dd92cdca56c966a5a9f899a8d026ce6677d5c61c
Carmen Pérez <carmenp.gomez99@gmail.com>
2022-02-11 14:41:45 +0100

    feat(link wouter): using link to SPA

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './App.css'
 import ListOfGifs from './components/ListOfGifs'
-import {Route} from 'wouter'
+import {Route, Link} from 'wouter'
 
 export default function App() {
   return (
     <div className="App">
       <section className="App-content">
         <h1>App</h1>
-        <a href='/gif/panda'>Gifs pandas</a>
-        <a href='/gif/morty'>Gifs morty</a>
-        <a href='/gif/rick'>Gifs rick</a>
+        <Link to='/gif/panda'>Gifs pandas</Link>
+        <Link to='/gif/morty'>Gifs morty</Link>
+        <Link to='/gif/rick'>Gifs rick</Link>
         <Route
           component={ListOfGifs}
           path="/gif/:keyword" />
