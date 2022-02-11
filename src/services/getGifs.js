9ba922957ecbe75c62f4e90a1a83ef09d80b2c1d
Carmen Pérez <carmenp.gomez99@gmail.com>
2022-02-11 14:52:57 +0100

    feat(services): add condition

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,14 +1,17 @@
 const api_key ='jDlQHAfwGVaDCuG8z634xFDmiZ897uXn'
 
 export default async function getGifs({keyword = 'morty'} = {}) {
-    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${keyword}&limit=7&offset=0&rating=g&lang=en`;
+    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${keyword}&limit=7&offset=0&rating=g&lang=en`
+
     const res = await fetch(apiURL)
     const response = await res.json()
-    const { data } = response;
-    const gifs = data.map(image => {
-        const {images, title, id} = image
-        const { url } = images.downsized_medium
-        return {title, id, url}
-    });
-    return gifs;
+    const { data = [] } = response;
+    if (Array.isArray(data)) {
+        const gifs = data.map(image => {
+            const {images, title, id} = image
+            const { url } = images.downsized_medium
+            return {title, id, url}
+        })
+        return gifs
+    }
 }
