5dfa7ea314e9dcd540253f3a1a2862318af276b7
Carmen Pérez <carmenp.gomez99@gmail.com>
2022-02-11 15:02:32 +0100

    feat(loading)

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -5,13 +5,19 @@ import getGifs from '../services/getGifs'
 export default function ListOfGifs ({ params }) {
     const { keyword } = params
 
+    const [loading, setLoading] = useState(false)
     const [gifs, setGifs] = useState([])
 
     useEffect(function () {
+        setLoading(true)
         getGifs({ keyword })
-            .then(gifs => setGifs(gifs))
+            .then(gifs => {
+                setGifs(gifs)
+                setLoading(false)
+            })
       }, [keyword])
 
+    if(loading) return <span>Loading...</span>
 
     return <>
         {
